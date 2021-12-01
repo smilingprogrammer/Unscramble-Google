a63ef45f77a1f17178860f65e1b2f58e0206f869
smilingprogrammer <abdulsoburoyewale@gmail.com>
2021-12-01 20:26:59 +0100

    ViewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,26 +1,69 @@
 package com.example.android.unscramble.ui.game
 
 import android.util.Log
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.ViewModel
+import com.example.android.unscramble.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-class GameViewModel: ViewModel(){
+class GameViewModel: ViewModel() {
+    init {
+        Log.d("GameFragment", "GameViewModel Created")
+        getNextWord()
+    }
+
     init {
         Log.d("GameViewModel", "GameViewModel Create!")
     }
 
-    private var score = 0
-    private var currentWordCount = 0
-    private var _currentScrambledWord = "text"
+    private var _score = 0
+    val score: Int
+        get() = _score
 
-    private var _count = 0
-    val count: Int
-    get() = _count
+    private var _currentWordCount = 0
+    val currentWordCount: Int
+        get() = _currentWordCount
 
+    private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
-    get() = _currentScrambledWord
+        get() = _currentScrambledWord
+
+
+    private var wordList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
 
     override fun onCleared() {
         super.onCleared()
         Log.d("GameViewModel", "GameViewModel Created")
     }
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        var tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+
+        if (wordList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++_currentWordCount
+            wordList.add(currentWord)
+        }
+    }
+
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
+//    private fun showFinalScoreDialog() {
+//        MaterialAlertDialogBuilder(requireContext())
+//            .setTitle(getString.congratulations)
+//    }
 }
